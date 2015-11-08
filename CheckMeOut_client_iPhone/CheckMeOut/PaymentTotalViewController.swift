949f325d88e98f5bfc84066f13cfce85cc721ae4
Ray <RuichengXian@Ruichengs-MacBook-Pro.local>
2015-11-07 21:05:43 -0500

    w/ Paypal

--- CheckMeOut_client_iPhone/CheckMeOut/PaymentTotalViewController.swift
+++ CheckMeOut_client_iPhone/CheckMeOut/PaymentTotalViewController.swift
@@ -8,7 +8,32 @@
 
 import UIKit
 
-class PaymentTotalViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class PaymentTotalViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, PayPalPaymentDelegate {
+    var environment:String = PayPalEnvironmentNoNetwork {
+        willSet(newEnvironment) {
+            if (newEnvironment != environment) {
+                PayPalMobile.preconnectWithEnvironment(newEnvironment)
+            }
+        }
+    }
+    
+    #if HAS_CARDIO
+    var acceptCreditCards: Bool = true {
+    didSet {
+    payPalConfig.acceptCreditCards = acceptCreditCards
+    }
+    }
+    #else
+    var acceptCreditCards: Bool = false {
+        didSet {
+            payPalConfig.acceptCreditCards = acceptCreditCards
+        }
+    }
+    #endif
+    
+    var resultText = "" // empty
+    var payPalConfig = PayPalConfiguration() // default
+    
     
     
     
@@ -18,9 +43,9 @@ class PaymentTotalViewController: UIViewController, UITableViewDelegate, UITable
     
     
     //////////////
-    var exp = [["Hoodie Pillow", "DigiStore", "$9.99"], ["LED Keychain", "DigiStore", "$9.55"]]
+    let exp = [["Hoodie Pillow", "DigiStore", "$9.99"], ["LED Keychain", "DigiStore", "$9.55"]]
     
-    
+
     
     var groupList = [String]()
     
@@ -42,11 +67,16 @@ class PaymentTotalViewController: UIViewController, UITableViewDelegate, UITable
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cellIdentifier = "PaymentTotalViewControllerTableViewCell"
-
+        
         let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! PaymentTotalViewControllerTableViewCell
+        
         
         ///////////////
         let textexp = exp[indexPath.row]
+        
+        
+        
+        
         
         cell.productName.text = textexp[0]
         cell.productPrice.text = textexp[2]
@@ -64,11 +94,21 @@ class PaymentTotalViewController: UIViewController, UITableViewDelegate, UITable
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
+        
         tableView.delegate = self
         tableView.dataSource = self
         self.tableView.reloadData()
-
+        
+        
+        
+        payPalConfig.acceptCreditCards = acceptCreditCards;
+        payPalConfig.merchantName = "Awesome Shirts, Inc."
+        payPalConfig.merchantPrivacyPolicyURL = NSURL(string: "https://www.paypal.com/webapps/mpp/ua/privacy-full")
+        payPalConfig.merchantUserAgreementURL = NSURL(string: "https://www.paypal.com/webapps/mpp/ua/useragreement-full")
+        payPalConfig.languageOrLocale = NSLocale.preferredLanguages()[0]
+        payPalConfig.payPalShippingAddressOption = .PayPal;
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -81,6 +121,131 @@ class PaymentTotalViewController: UIViewController, UITableViewDelegate, UITable
     @IBAction func addMore(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        PayPalMobile.preconnectWithEnvironment(environment)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // PayPalPaymentDelegate
+    
+    func payPalPaymentDidCancel(paymentViewController: PayPalPaymentViewController!) {
+        print("PayPal Payment Cancelled")
+        resultText = ""
+        paymentViewController?.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func payPalPaymentViewController(paymentViewController: PayPalPaymentViewController!, didCompletePayment completedPayment: PayPalPayment!) {
+        print("PayPal Payment Success !")
+        paymentViewController?.dismissViewControllerAnimated(true, completion: { () -> Void in
+            // send completed confirmaion to your server
+            print("Here is your proof of payment:\n\n\(completedPayment.confirmation)\n\nSend this to your server for confirmation and fulfillment.")
+            
+            self.resultText = completedPayment!.description
+        })
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    @IBAction func buyClothingAction(sender: AnyObject) {
+        // Remove our last completed payment, just for demo purposes.
+        resultText = ""
+        
+        // Note: For purposes of illustration, this example shows a payment that includes
+        //       both payment details (subtotal, shipping, tax) and multiple items.
+        //       You would only specify these if appropriate to your situation.
+        //       Otherwise, you can leave payment.items and/or payment.paymentDetails nil,
+        //       and simply set payment.amount to your total charge.
+        
+        // Optional: include multiple items
+        let item1 = PayPalItem(name: "Old jeans with holes", withQuantity: 2, withPrice: NSDecimalNumber(string: "84.99"), withCurrency: "USD", withSku: "Hip-0037")
+        let item2 = PayPalItem(name: "Free rainbow patch", withQuantity: 1, withPrice: NSDecimalNumber(string: "0.00"), withCurrency: "USD", withSku: "Hip-00066")
+        let item3 = PayPalItem(name: "Long-sleeve plaid shirt (mustache not included)", withQuantity: 1, withPrice: NSDecimalNumber(string: "37.99"), withCurrency: "USD", withSku: "Hip-00291")
+        
+        let items = [item1, item2, item3]
+        let subtotal = PayPalItem.totalPriceForItems(items)
+        
+        // Optional: include payment details
+        let shipping = NSDecimalNumber(string: "5.99")
+        let tax = NSDecimalNumber(string: "2.50")
+        let paymentDetails = PayPalPaymentDetails(subtotal: subtotal, withShipping: shipping, withTax: tax)
+        
+        let total = subtotal.decimalNumberByAdding(shipping).decimalNumberByAdding(tax)
+        
+        let payment = PayPalPayment(amount: total, currencyCode: "USD", shortDescription: "Hipster Clothing", intent: .Sale)
+        
+        payment.items = items
+        payment.paymentDetails = paymentDetails
+        
+        if (payment.processable) {
+            let paymentViewController = PayPalPaymentViewController(payment: payment, configuration: payPalConfig, delegate: self)
+            presentViewController(paymentViewController, animated: true, completion: nil)
+            //////////////////////////////
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        }
+        else {
+            // This particular payment will always be processable. If, for
+            // example, the amount was negative or the shortDescription was
+            // empty, this payment wouldn't be processable, and you'd want
+            // to handle that here.
+            print("Payment not processalbe: \(payment)")
+        }
+        
+    }
+    
     
     
     
